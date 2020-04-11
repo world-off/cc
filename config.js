121373a06af7a0193f6b13d663c6e0b9ceca9539
world-off <63488107+world-off@users.noreply.github.com>
2020-04-11 07:04:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-exports.TOKEN = 'Njk2NjQxNjkzNTQ5NTkyNjM3.XorsKA._-l58pTqMbAJxyKQ10rGpLK8M-s';
+exports.TOKEN = 'ton token';
 exports.PREFIX = '1';
 
 exports.GOOGLE_API_KEY = 'AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w';
